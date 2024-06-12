ad2456e715514929a353d30c7adf1a084f36348f
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-12 22:41:36 +0800

    munch: Setup DeviceAsWebcam
    Change-Id: Icf8e10cd21c4eef9a66af624971ffe9c4d39d992

--- device.mk
+++ device.mk
@@ -277,6 +277,7 @@ TARGET_SUPPORTS_OMX_SERVICE := false
 PRODUCT_PACKAGES += \
     ApertureOverlayDevice \
     CarrierConfigOverlayDevice \
+    DeviceAsWebcamResDevice \
     FrameworkResOverlayDevice \
     LineageDialerOverlayDevice \
     LineageSDKOverlayDevice \
